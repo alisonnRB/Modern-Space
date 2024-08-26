0ea9a3a0961654248a3467e91ff93b74d188b99b
alisonnRB <alisonrbjs@gmail.com>
2024-08-26 14:44:48 -0300

    init project

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors : {
+        "bg" : "#DFDED9",
+        "main" : "#8F9484",
+        "dark" : "#22211F",
+        "motion" : "#CBCAC5"
+      }
+    },
+  },
+  plugins: [],
+};
